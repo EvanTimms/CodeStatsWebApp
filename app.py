d1c025c35d2c82a15e2f2ed65057fe613f27608c
Evan <timmsevan@gmail.com>
2018-03-30 10:41:34 -0600

    links working!

--- app.py
+++ app.py
@@ -6,8 +6,24 @@ app = Flask(__name__)
 
 #adding routing decorator
 @app.route('/')
-def main():
+def index():
     return render_template('index.html')
+
+@app.route('/index')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/signIn')
+def signIn():
+    return render_template('signIn.html')
+
+@app.route('/signUp')
+def singUp():
+    return render_template('signUp.html')
 
 if __name__ == "__main__":
     app.run(debug = True)
