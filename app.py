23c31658c074a6abbf037a19916c8c31a283cf6a
Evan <timmsevan@gmail.com>
2018-03-30 11:45:19 -0600

    commented+added embedded chart to home page

--- app.py
+++ app.py
@@ -1,26 +1,48 @@
 from flask import Flask, render_template
-
+from data import personalInfo
+import pygal
 #initalizing application as instance of flask class
 
 app = Flask(__name__)
+personal_info = personalInfo()
 
-#adding routing decorator
+#build chart using pygal, will convert to query from database in future
+def build_line_chart():
+    line_chart = pygal.Line()
+    line_chart.title = 'Browser usage evolution (in %)'
+    line_chart.x_labels = map(str, range(2002, 2013))
+    line_chart.add('Firefox', [None, None,    0, 16.6,   25,   31, 36.4, 45.5, 46.3, 42.8, 37.1])
+    line_chart.add('Chrome',  [None, None, None, None, None, None,    0,  3.9, 10.8, 23.8, 35.3])
+    line_chart.add('IE',      [85.8, 84.6, 84.7, 74.5,   66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
+    line_chart.add('Others',  [14.2, 15.4, 15.3,  8.9,    9, 10.4,  8.9,  5.8,  6.7,  6.8,  7.5])
+    line_chart = line_chart.render_data_uri()
+    return line_chart
+
+#adding routing decorator for intial bootup
 @app.route('/')
 def index():
-    return render_template('index.html')
+    #builds charts
+    chart = build_line_chart()
+    #renders html web page
+    return render_template('index.html', profile = personal_info, chart = chart)
 
+#home route
 @app.route('/index')
 def home():
-    return render_template('index.html')
+    chart = build_line_chart()
+    return render_template('index.html', profile = personal_info, chart = chart)
 
+#about route
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+#signin route
 @app.route('/signIn')
 def signIn():
     return render_template('signIn.html')
 
+#sign up route
 @app.route('/signUp')
 def singUp():
     return render_template('signUp.html')
---
+++ data.py
@@ -0,0 +1,16 @@
+'''database python module, will be used to display all user data'''
+
+def personalInfo():
+    #load from database
+    
+    info = [
+        {
+            'name': 'Evan',
+            'degree': 'Computer Engineering',
+            'year': 'Second Year',
+            'gpa': 0,
+            'email': 'timmmsevan@gmail.com',
+            'github': 'https://github.com/EvanTimms'
+        }
+    ]
+    return info
